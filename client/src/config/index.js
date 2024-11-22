b9e2b138cc1d7a25a5b374a004a1d0b75a2cc280
endyelg <galaponangelrubia@gmail.com>
2024-11-22 17:32:35 +0800

    form validation-user login

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,7 +1,7 @@
 export const registerFormControls = [
   {
     name: "userName",
-    label: "User Name",
+    label: "Username",
     placeholder: "Enter your user name",
     componentType: "input",
     type: "text",
--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,10 +1,20 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import CommonForm from "@/components/common/form";
 import { useToast } from "@/components/ui/use-toast";
 import { loginFormControls } from "@/config";
 import { loginUser } from "@/store/auth-slice";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required.")
+    .email("Email is not valid."),
+  password: Yup.string()
+    .required("Password is required.")
+    .min(6, "Password must be at least 6 characters"),
+});
 
 const initialState = {
   email: "",
@@ -12,26 +22,8 @@ const initialState = {
 };
 
 function AuthLogin() {
-  const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const { toast } = useToast();
-
-  function onSubmit(event) {
-    event.preventDefault();
-
-    dispatch(loginUser(formData)).then((data) => {
-      if (data?.payload?.success) {
-        toast({
-          title: data?.payload?.message,
-        });
-      } else {
-        toast({
-          title: data?.payload?.message,
-          variant: "destructive",
-        });
-      }
-    });
-  }
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6">
@@ -49,15 +41,44 @@ function AuthLogin() {
           </Link>
         </p>
       </div>
-      <CommonForm
-        formControls={loginFormControls}
-        buttonText={"Sign In"}
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={onSubmit}
-      />
+      <Formik
+        initialValues={initialState}
+        validationSchema={validationSchema}
+        onSubmit={async (formData) => {
+          const data = await dispatch(loginUser(formData));
+          if (data?.payload?.success) {
+            toast({
+              title: data?.payload?.message,
+            });
+          } else {
+            toast({
+              title: data?.payload?.message,
+              variant: "destructive",
+            });
+          }
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            {loginFormControls.map((controlItem) => (
+              <div className="grid w-full gap-1.5" key={controlItem.name}>
+                <label className="mb-1">{controlItem.label}</label>
+                <Field
+                  name={controlItem.name}
+                  placeholder={controlItem.placeholder}
+                  type={controlItem.type}
+                  className={`input ${errors[controlItem.name] && touched[controlItem.name] ? 'border-red-500' : ''}`}
+                />
+                <ErrorMessage name={controlItem.name} component="p" className="text-red-500 text-sm" />
+              </div>
+            ))}
+            <button type="submit" className="mt-2 w-full">
+              Sign In
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
-
 export default AuthLogin;
